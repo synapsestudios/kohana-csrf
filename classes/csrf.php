f591ccdcdba8958e17ef95f80a83198c98bf96c4
Lorenzo Pisani <zeelot3k@gmail.com>
2010-03-26 18:05:29 -0700

    initial commit

---
+++ classes/csrf.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class CSRF {
+
+	public static function token()
+	{
+		$token = Session::instance()->get('csrf-token');
+		! $token AND $token = Text::random('alnum', 10);
+		Session::instance()->set('csrf-token', $token);
+
+		return $token;
+	}
+
+	public static function valid($token)
+	{
+		return $token === self::token();
+	}
+
+}
