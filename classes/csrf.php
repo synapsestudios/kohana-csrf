7c5a8d55258ce8c6a1927a4b31182254b45bc23b
Lorenzo Pisani <zeelot3k@gmail.com>
2010-07-15 04:41:42 -0700

    implementing csrf namespaces... if you have multiple forms on a page, they will both get a unique csrf and be able to validate each... one csrf will not invalidate the other usage: CSRF::token('login'); CSRF::token('register'); // custom expiration CSRF::token('login', '+2 minutes'); // defaults to 5 minutes

--- classes/csrf.php
+++ classes/csrf.php
@@ -2,31 +2,46 @@
 
 class CSRF {
 
+	public static $expiration = '+5 minutes';
+
 	/**
 	 * Returns the token in the session or generates a new one
 	 *
-	 * @return string
+	 * @param   string  $namespace - semi-unique name for the token (support for multiple forms)
+	 * @param   mixed   $expiration - a timestamp or a strtotime-able string (ex: '5 minutes', defaults to self::$expiration
+	 * @param   bool    $new - Wether or not to forcefully create a new token
+	 * @return  string
 	 */
-	public static function token($new = FALSE)
+	public static function token($namespace = 'default', $expiration = NULL, $new = FALSE)
 	{
-		$token = Session::instance()->get('csrf-token');
-		if ( ! $token OR $new)
+		($expiration === NULL) AND $expiration = self::$expiration;
+		( ! is_int($expiration)) AND $expiration = strtotime($expiration);
+
+		$token = Session::instance()->get('csrf-token-'.$namespace);
+
+		// Generate a new token if no token is found, $new is true, or the current one expired
+		if ( ! $token OR $new OR $token['expires'] < time())
 		{
-			$token = Text::random('alnum', 10);
-			Session::instance()->set('csrf-token', $token);
+			$token = array
+			(
+				'token'   => Text::random('alnum', 10),
+				'expires' => strtotime(self::$expiration),
+			);
+			Session::instance()->set('csrf-token-'.$namespace, $token);
 		}
 
-		return $token;
+		return $token['token'];
 	}
 
 	/**
 	 * Validation rule for checking a valid token
 	 *
-	 * @return bool
+	 * @param   string  $namespace - the semi-unique identifier specified when getting the token
+	 * @param   string  $token - the token string to check for
+	 * @return  bool
 	 */
-	public static function valid($token)
+	public static function valid($namespace = 'default', $token)
 	{
-		return $token === self::token();
+		return $token === self::token($namespace);
 	}
-
 }
