8d4588471bd232dff9ad3b55dc9a8025be5e7ecc
Lorenzo Pisani <zeelot3k@gmail.com>
2010-08-13 14:44:45 -0700

    removing token expiration and namespaces. one token per session is our new convention. if anyone knows of a valid security risk with this, submit an issue please.

--- classes/csrf.php
+++ classes/csrf.php
@@ -2,32 +2,21 @@
 
 class CSRF {
 
-	public static $expiration = '+5 minutes';
-
 	/**
 	 * Returns the token in the session or generates a new one
 	 *
 	 * @param   string  $namespace - semi-unique name for the token (support for multiple forms)
-	 * @param   mixed   $expiration - a timestamp or a strtotime-able string (ex: '5 minutes', defaults to self::$expiration
-	 * @param   bool    $new - Wether or not to forcefully create a new token
 	 * @return  string
 	 */
-	public static function token($namespace = 'default', $expiration = NULL, $new = FALSE)
+	public static function token()
 	{
-		($expiration === NULL) AND $expiration = self::$expiration;
-		( ! is_int($expiration)) AND $expiration = strtotime($expiration);
+		$token = Session::instance()->get('csrf-token');
 
-		$token = Session::instance()->get('csrf-token-'.$namespace);
-
-		// Generate a new token if no token is found, $new is true, or the current one expired
-		if ( ! $token OR $new OR $token['expires'] < time())
+		// Generate a new token if no token is found
+		if ( ! $token)
 		{
-			$token = array
-			(
-				'token'   => Text::random('alnum', 10),
-				'expires' => strtotime(self::$expiration),
-			);
-			Session::instance()->set('csrf-token-'.$namespace, $token);
+			$token = Text::random('alnum', rand(20, 30)),
+			Session::instance()->set('csrf-token', $token);
 		}
 
 		return $token['token'];
@@ -36,11 +25,10 @@ class CSRF {
 	/**
 	 * Validation rule for checking a valid token
 	 *
-	 * @param   string  $namespace - the semi-unique identifier specified when getting the token
 	 * @param   string  $token - the token string to check for
 	 * @return  bool
 	 */
-	public static function valid($namespace = 'default', $token)
+	public static function valid($token)
 	{
 		return $token === self::token($namespace);
 	}
